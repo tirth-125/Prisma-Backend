5c783ecd5ff121b9e9ee571bab8a98c1ef614240
tirth-125 <stirth125@gmail.com>
2024-03-15 17:24:06 +0530

    so now i lern about what is the filtering and what is the pagination

--- Controller/postController.js
+++ Controller/postController.js
@@ -20,8 +20,20 @@ export const createPost = async (req, res) => {
 }
 
 export const getAllPost = async (req, res) => {
+
     try {
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 10;
+        if (page <= 0) {
+            page = 1
+        }
+        if (limit <= 0 && limit > 100) {
+            limit = 10;
+        }
+        const skip = (page - 1) * limit;
         const posts = await prisma.post.findMany({
+            skip: skip,
+            take: limit,
             include: {
                 Comment: {
                     include: {
@@ -34,16 +46,37 @@ export const getAllPost = async (req, res) => {
                     }
                 }
             },
-            orderBy : {
-                id:'asc'
+            orderBy: {
+                id: 'asc'
+            },
+            where: {
+                NOT: [
+                    {
+                        title: {
+                            startsWith: "So"
+                        }
+                    }, {
+                        title: {
+                            endsWith: "in Reality"
+                        }
+                    }
+
+
+                ]
             }
         });
-
+        const totalPosts = await prisma.post.count();
+        const totalPages = Math.ceil(totalPosts / limit);
         return res.status(200).json({
             success: true,
             length: posts.length,
             data: {
                 posts
+            },
+            meta: {
+                totalPosts,
+                currentPage: page,
+                totalPages,
             }
         });
     } catch (error) {
@@ -128,4 +161,28 @@ export const deletePost = async (req, res) => {
             message: error.message
         });
     }
+}
+export const searchPost = async (req, res) => {
+    try {
+        const query = req.params.query;
+        // console.log(query, "hgj");
+        const posts = await prisma.post.findMany({
+            where: {
+                description: {
+                    search: query
+                }
+            }
+        });
+        return res.status(200).json({
+            data: {
+                posts
+            }
+        });
+    } catch (error) {
+        return res.status(200).json({
+            success: false,
+            message: error.message
+        });
+    }
+
 }
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import {createPost,getAllPost,showPost,postUpdate,deletePost} from '../Controller/postController.js';
+import {createPost,getAllPost,showPost,postUpdate,deletePost,searchPost} from '../Controller/postController.js';
 
 const route = express.Router();
-  
+   
 route.route('/').get(getAllPost);
+route.route('/search').get(searchPost);
 route.route('/').post(createPost);
+
   
 route.route('/:id').put(postUpdate);
 route.route('/:id').get(showPost);
